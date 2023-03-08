24cce727b85d96ffbdf2c7d5d36b8798339e3e80
Mk Asik <mkasik123@gmail.com>
2023-03-08 19:38:49 +0600

    added

---
+++ src/pages/[...postpath].tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { GraphQLClient, gql } from 'graphql-request';
+import styles from '../styles/style.module.css';
+
+
+
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+    const graphQLClient = new GraphQLClient(endpoint);
+    const referringURL = ctx.req.headers?.referer || null;
+    const pathArr = ctx.query.postpath as Array<string>;
+    const path = pathArr.join('/');
+    const fbclid = ctx.query.fbclid;
+
+    // redirect if facebook is the referer or request contains fbclid
+    if (referringURL?.includes('facebook.com') || fbclid) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: `${endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(
+                    path as string
+                )}`,
+            },
+        };
+    }
+
+    const query = gql`
+    {
+      post(id: "/${path}/", idType: URI) {
+        id
+        excerpt
+        title
+        link
+        dateGmt
+        modifiedGmt
+        content
+        author {
+          node {
+            name
+          }
+        }
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+      }
+    }
+  `;
+
+    const data = await graphQLClient.request(query);
+
+    if (!data.post) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            path,
+            post: data.post,
+            host: ctx.req.headers.host,
+        },
+    };
+};
+
+
+interface PostProps {
+    post: any;
+    host: string;
+    path: string;
+}
+
+const Post: React.FC<PostProps> = (props) => {
+    const { post, host, path } = props;
+
+    // to remove tags from excerpt
+    const removeTags = (str: string) => {
+        if (str === null || str === '') return '';
+        else str = str.toString();
+        return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+    };
+
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content={post.title} />
+                <link rel="canonical" href={`https://${host}/${path}`} />
+                <meta property="og:description" content={removeTags(post.excerpt)} />
+                <meta property="og:url" content={`https://${host}/${path}`} />
+                <meta property="og:type" content="article" />
+                <meta property="og:locale" content="en_US" />
+                {post.featuredImage && (
+                    <>
+                        <meta
+                            property="og:image"
+                            content={post.featuredImage.node.sourceUrl}
+                        />
+                        <meta
+                            property="og:image:alt"
+                            content={post.featuredImage.node.altText}
+                        />
+                    </>
+                )}
+            </Head>
+            <div>
+                <h1>{post.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+            </div>
+
+        </>
+    );
+};
+
+export default Post;
